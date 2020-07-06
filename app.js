3cbeed4113b4c2cd53de912ae368742f492ff1ba
Ibrahim Kurhan <ikurhan@outlook.com>
2020-07-06 20:04:57 +0100

    margin has been changed

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 let thumbnails = document.getElementsByClassName("thumbnail");
 let activeImages = document.getElementsByClassName("active");
 
-for (var i = 0; i < thumbnails.length; i++) {
-  thumbnails[i].addEventListener("click", function () {
+for (let i = 0; i < thumbnails.length; i++) {
+  thumbnails[i].addEventListener("click", () => {
     if (activeImages.length > 0) {
       activeImages[0].classList.remove("active");
     }
@@ -12,6 +12,27 @@ for (var i = 0; i < thumbnails.length; i++) {
     document.getElementById("featured").src = this.src;
   });
 }
+
+// count time
+let countDownDate = new Date("July 6, 2020 23:50:25").getTime();
+
+let x = setInterval(() => {
+  let now = new Date().getTime();
+
+  let distance = countDownDate - now;
+
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("demo").innerHTML =
+    hours + "h " + minutes + "m " + seconds + "s ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
 // card validation
 const form = document.getElementById("form");
 const username = document.getElementById("username");
@@ -121,22 +142,3 @@ function isEmail(email) {
     email
   );
 }
-var countDownDate = new Date("July 6, 2020 23:50:25").getTime();
-
-var x = setInterval(function () {
-  var now = new Date().getTime();
-
-  var distance = countDownDate - now;
-
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("demo").innerHTML =
-    hours + "h " + minutes + "m " + seconds + "s ";
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
